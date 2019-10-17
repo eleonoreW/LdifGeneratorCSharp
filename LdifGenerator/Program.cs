5fb93460a270b8d7ff7437e1ac3bf8c15d83b11f
EleonoreW <el.pc.wald@gmail.com>
2019-10-17 15:30:02 +0200

    Fixed issues with Unix New line We can now choose between Windows "\r\n" or Unix "\n" for the line ends in the output files

--- LdifGenerator/Program.cs
+++ LdifGenerator/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Text;
-using McMaster.Extensions.CommandLineUtils;
 
 namespace LdifGenerator
 {
+    enum EnvironnementType { UNIX, WINDOWS }
+
     class Program
     {
         [Required]
@@ -21,14 +23,16 @@ namespace LdifGenerator
         [Option("-s|--size <SizeLimit>", "Specifies the Search size timeout.", CommandOptionType.SingleValue)]
         public int Size { get; } = 10;
 
+        [Option("--seed <SizeLimit>", "Specifies the Search size timeout.", CommandOptionType.SingleValue)]
+        public int Seed { get; } = -1;
+
         [Option("-m|--maxLineNumber <linesNumber>", "Specifies the Search size timeout.", CommandOptionType.SingleValue)]
         public int MaxFileSize { get; set; } = -1;
 
         [Option("-l|--EOL ", "Specifies if we must use Windows EOL.", CommandOptionType.SingleValue)]
-        public bool EOL { get; set; } = false;
+        public EnvironnementType Environnement { get; set; } = EnvironnementType.UNIX;
 
-        public string eol { get; set; } = "\r";
-
+        string EOL;
         static int Main(string[] args)
         {
 #if DEBUG
@@ -36,7 +40,8 @@ namespace LdifGenerator
             {
                 "-b=dc=example,dc=com",
                 "-o=C:/Temp/generated",
-                "-s=1000",
+                "-s=2000",
+                //"--seed=2",
                 //"-m=300"
             };
 #endif
@@ -45,8 +50,19 @@ namespace LdifGenerator
 
         private void OnExecute(CommandLineApplication app)
         {
+
+
             try
             {
+                switch (Environnement)
+                {
+                    case EnvironnementType.UNIX:
+                        EOL = "\n";
+                        break;
+                    case EnvironnementType.WINDOWS:
+                        EOL = "\r\n";
+                        break;
+                }
                 string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\Data\";
 
                 string[] OUNames = File.ReadAllLines(path + "organizational-units.txt");
@@ -58,9 +74,9 @@ namespace LdifGenerator
                 string[] ranks = File.ReadAllLines(path + "title-ranks.txt");
 
                 List<string> DNs = new List<string>();
-                Random random = new Random(2);
+                Random random = Seed != -1 ? new Random(Seed) : new Random();
 
-                double nbFile=1;
+                double nbFile = 1;
                 if (MaxFileSize != -1)
                 {
                     nbFile = Math.Ceiling((float)Size / (float)MaxFileSize);
@@ -70,11 +86,12 @@ namespace LdifGenerator
                     MaxFileSize = Size;
                 }
 
-                for (int j=0; j < nbFile; j = j+1)
+                for (int j = 0; j < nbFile; j = j + 1)
                 {
-                    using (var writer = new StreamWriter(new FileStream(OutputFile + "_"+j+".ldif", FileMode.Create)))
+                    using (var writer = new StreamWriter(new FileStream(OutputFile + "_" + j + ".ldif", FileMode.Create)))
                     {
-                        int max = (j == nbFile-1 && MaxFileSize != Size) ? Size % MaxFileSize : MaxFileSize;
+                        writer.NewLine = EOL;
+                        int max = (j == nbFile - 1 && MaxFileSize != Size) ? Size % MaxFileSize : MaxFileSize;
                         for (int i = 1; i <= max; i += 1)
                         {
                             var id = i + MaxFileSize * j;
@@ -84,39 +101,40 @@ namespace LdifGenerator
                             DNs.Add(dn);
 
                             string host = mailhosts[random.Next(0, mailhosts.Length)];
-                            writer.Write(GeneratePerson(dn, name, personClasses, ou, host) + "\r");
+                            writer.WriteLine(GeneratePerson(dn, name, personClasses, ou, host));
                         }
                     }
                 }
 
                 using (var writer = new StreamWriter(new FileStream(OutputFile + "_ou.ldif", FileMode.Create)))
                 {
-                    foreach(string entry in GenerateOU(BaseDN, OUNames))
+                    writer.NewLine = EOL;
+                    foreach (string entry in GenerateOU(BaseDN, OUNames))
                     {
-                        writer.Write(entry + "\r");
+                        writer.WriteLine(entry);
                     }
-
-                    
                 }
 
                 using (var writer = new StreamWriter(new FileStream(OutputFile + "_mod.ldif", FileMode.Create)))
                 {
-                    foreach(string dn in DNs)
+                    writer.NewLine = EOL;
+                    foreach (string dn in DNs)
                     {
                         string title = ranks[random.Next(0, ranks.Length)] + " " + positions[random.Next(0, positions.Length)];
-                        writer.Write(ModifyPerson(dn,title)+"\r");
+                        writer.WriteLine(ModifyPerson(dn, title));
                     }
                 }
 
                 using (var writer = new StreamWriter(new FileStream(OutputFile + "_del.ldif", FileMode.Create)))
                 {
+                    writer.NewLine = EOL;
                     foreach (string dn in DNs)
                     {
-                        writer.Write(DeletePerson(dn) + "\r");
+                        writer.WriteLine(DeletePerson(dn));
                     }
                     foreach (string entry in DeleteOU(BaseDN, OUNames))
                     {
-                        writer.Write(entry + "\r");
+                        writer.WriteLine(entry);
                     }
                 }
             }
@@ -136,53 +154,54 @@ namespace LdifGenerator
         private string ModifyPerson(string dn, string title)
         {
             var builder = new StringBuilder();
-            builder.AppendLine("dn: " + dn);
-            builder.AppendLine("changetype: modify");
-            builder.AppendLine("add: description");
-            builder.AppendLine("description: " + title);
+            builder.Append("dn: " + dn + EOL);
+            builder.Append("changetype: modify" + EOL);
+            builder.Append("add: description" + EOL);
+            builder.Append("description: " + title + EOL);
 
             return builder.ToString();
         }
+
         private string GeneratePerson(string dn, string name, string[] classes, string ou, string host)
         {
             var rand = new Random();
             var builder = new StringBuilder();
-            builder.AppendLine("dn: " + dn);
-            builder.AppendLine("changetype: add");
-            builder.AppendLine("cn: " + name);
-            builder.AppendLine("sn: " + name);
+            builder.Append("dn: " + dn + EOL);
+            builder.Append("changetype: add" + EOL);
+            builder.Append("cn: " + name + EOL);
+            builder.Append("sn: " + name + EOL);
             var pClass = classes[rand.Next(1, classes.Length)];
             if (pClass.Equals("organizationalPerson"))
             {
-                builder.AppendLine("ou: " + ou);
+                builder.Append("ou: " + ou + EOL);
             }
             if (pClass.Equals("inetOrgPerson"))
             {
-                builder.AppendLine("ou: " + ou);
-                builder.AppendLine(("mail: " + name.Replace(' ', '.') + "@" + ou.Replace(" ", String.Empty) + "." + host).ToLowerInvariant());
+                builder.Append("ou: " + ou + EOL);
+                builder.Append(("mail: " + name.Replace(' ', '.') + "@" + ou.Replace(" ", String.Empty) + "." + host).ToLowerInvariant() + EOL);
             }
-            builder.AppendLine("objectclass: " + pClass);
+            builder.Append("objectclass: " + pClass + EOL);
             return builder.ToString();
         }
 
         private string DeletePerson(string dn)
         {
             var builder = new StringBuilder();
-            builder.AppendLine(dn);
+            builder.Append(dn + EOL);
             return builder.ToString();
         }
 
-       
+
 
         private List<string> GenerateOU(string baseDN, string[] ouNames)
         {
             List<string> myList = new List<string>();
-            foreach(string name in ouNames)
+            foreach (string name in ouNames)
             {
                 var builder = new StringBuilder();
-                builder.AppendLine("dn: ou=" + name + "," + baseDN);
-                builder.AppendLine("objectclass: organizationalUnit");
-                builder.AppendLine("ou: " + name);
+                builder.Append("dn: ou=" + name + "," + baseDN + EOL);
+                builder.Append("objectclass: organizationalUnit" + EOL);
+                builder.Append("ou: " + name + EOL);
                 myList.Add(builder.ToString());
             }
             return myList;
@@ -193,7 +212,7 @@ namespace LdifGenerator
             foreach (string name in ouNames)
             {
                 var builder = new StringBuilder();
-                builder.AppendLine("ou=" + name + "," + baseDN);
+                builder.Append("ou=" + name + "," + baseDN + EOL);
                 myList.Add(builder.ToString());
             }
             return myList;
